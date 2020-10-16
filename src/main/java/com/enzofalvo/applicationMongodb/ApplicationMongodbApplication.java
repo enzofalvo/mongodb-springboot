77366fc6280c4889293dcf72e19dfb52eb698061
Enzo Falvo <enzo.fvo@gmail.com>
2020-10-15 22:34:11 -0300

    Project created

---
+++ src/main/java/com/enzofalvo/applicationMongodb/ApplicationMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.enzofalvo.applicationMongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enzofalvo/applicationMongodb/ApplicationMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzofalvo.applicationMongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
