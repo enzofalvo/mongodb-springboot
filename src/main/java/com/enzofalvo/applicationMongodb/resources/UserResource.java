e5eccfc891b35283134ea809848e6f76b8806fbf
Enzo Falvo <enzo.fvo@gmail.com>
2020-10-18 15:08:58 -0300

    Using DTO standard to return users

--- src/main/java/com/enzofalvo/applicationMongodb/resources/UserResource.java
+++ src/main/java/com/enzofalvo/applicationMongodb/resources/UserResource.java
@@ -1,10 +1,12 @@
 package com.enzofalvo.applicationMongodb.resources;
 
 import com.enzofalvo.applicationMongodb.domain.User;
+import com.enzofalvo.applicationMongodb.dto.UserDTO;
 import com.enzofalvo.applicationMongodb.services.UserService;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +21,11 @@ public class UserResource {
 
     //method to get all users with the endpoint
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity <List<User>> findAll() {
+    public ResponseEntity <List<UserDTO>> findAll() {
         //find in database with service
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        //convert obj in list user to listdto 
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
