11fc0168134bf48d489234142561a8b8a00ce3f4
Enzo Falvo <enzo.fvo@gmail.com>
2020-10-19 16:16:59 -0300

    Adding comments to posts

--- src/main/java/com/enzofalvo/applicationMongodb/config/Instantiation.java
+++ src/main/java/com/enzofalvo/applicationMongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.enzofalvo.applicationMongodb.config;
 import com.enzofalvo.applicationMongodb.domain.Post;
 import com.enzofalvo.applicationMongodb.domain.User;
 import com.enzofalvo.applicationMongodb.dto.AuthorDTO;
+import com.enzofalvo.applicationMongodb.dto.CommentDTO;
 import com.enzofalvo.applicationMongodb.repository.PostRepository;
 import com.enzofalvo.applicationMongodb.repository.UserRepository;
 import java.text.SimpleDateFormat;
@@ -38,6 +39,13 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+        
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+        
         postRepository.saveAll(Arrays.asList(post1, post2));
         
         maria.getPosts().addAll(Arrays.asList(post1, post2));
--- src/main/java/com/enzofalvo/applicationMongodb/domain/Post.java
+++ src/main/java/com/enzofalvo/applicationMongodb/domain/Post.java
@@ -2,8 +2,11 @@
 package com.enzofalvo.applicationMongodb.domain;
 
 import com.enzofalvo.applicationMongodb.dto.AuthorDTO;
+import com.enzofalvo.applicationMongodb.dto.CommentDTO;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -17,6 +20,7 @@ public class Post implements Serializable {
     private String body;
     
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
     
     public Post() {
     }
@@ -69,6 +73,14 @@ public class Post implements Serializable {
         this.author = author;
     }
 
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
+    
     @Override
     public int hashCode() {
         int hash = 5;
