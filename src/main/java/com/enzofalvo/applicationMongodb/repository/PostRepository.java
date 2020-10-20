90ac25c929ef2a00e606d933ece6671f5f488a37
Enzo Falvo <enzo.fvo@gmail.com>
2020-10-20 16:15:45 -0300

    Query with multiple criteria

--- src/main/java/com/enzofalvo/applicationMongodb/repository/PostRepository.java
+++ src/main/java/com/enzofalvo/applicationMongodb/repository/PostRepository.java
@@ -2,6 +2,7 @@
 package com.enzofalvo.applicationMongodb.repository;
 
 import com.enzofalvo.applicationMongodb.domain.Post;
+import java.util.Date;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
@@ -13,4 +14,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> SearchTitle(String text);
     
     List<Post> findByTitleContainingIgnoreCase(String text);
+    
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/enzofalvo/applicationMongodb/resources/PostResource.java
+++ src/main/java/com/enzofalvo/applicationMongodb/resources/PostResource.java
@@ -1,10 +1,9 @@
 package com.enzofalvo.applicationMongodb.resources;
 
 import com.enzofalvo.applicationMongodb.domain.Post;
-import com.enzofalvo.applicationMongodb.domain.User;
-import com.enzofalvo.applicationMongodb.dto.UserDTO;
 import com.enzofalvo.applicationMongodb.resources.util.URL;
 import com.enzofalvo.applicationMongodb.services.PostService;
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +31,21 @@ public class PostResource {
         text = URL.decodeParam(text);
         
         List<Post> list = service.findByTitle(text);
+        
+        return ResponseEntity.ok().body(list);
+    }
+    
+    @RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+    public ResponseEntity <List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+       
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        
+        List<Post> list = service.fullSearch(text, min, max);
         
         return ResponseEntity.ok().body(list);
     }
